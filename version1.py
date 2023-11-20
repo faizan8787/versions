81570687f3ac226c5c099f49b8cef26871b4f5ee
faizan8787 <muhammadfaizansgc@gmail.com>
2023-11-20 15:45:59 +0500

    Shared by Faizan

---
+++ version1.py
@@ -0,0 +1,15 @@
+
+import math
+def add(num1, num2):
+    return num1+num2
+def sub(num1, num2):
+    return num1-num2
+def mul(num1, num2):
+    return num1*num2
+def div(num1, num2):
+    return num1/num2
+def sqrt(num1):
+    return math.sqrt(num1)
+
+res=sqrt(25)
+print(res)
---
+++ version2.py
@@ -0,0 +1,1 @@
+# This version is specified for Hajra's Work.
